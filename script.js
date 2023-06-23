802c295a3c949da48b044c1cbb628749976986a6
hacerparacrecer <136462408+hacerparacrecer@users.noreply.github.com>
2023-06-23 15:34:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+// Llamar a la función getWeather() al cargar la página
+window.addEventListener('load', getWeather);
+
+function getWeather() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+      console.log("La geolocalización no es compatible con este navegador.");
+    }
+  }
+  
+  function success(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    var apiKey = "274345cccdcb1a7bddbf61a51ab7551f"; // Reemplaza "tu_api_key" con tu clave de API real
+  
+    var url =
+      "https://api.openweathermap.org/data/2.5/weather?lat=" +
+      latitude +
+      "&lon=" +
+      longitude +
+      "&units=metric&lang=es&appid=" +
+      apiKey;
+  
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        var weatherInfo = document.getElementById("weather-info");
+        weatherInfo.innerHTML =
+          "CLIMA" + "<br>" + "<br>" +
+          data.name + "<br>" +
+          Math.round(data.main.temp) + "°C<br>" +
+          data.weather[0].description;
+      })
+      .catch(error => {
+        console.log("Error al obtener los datos del clima:", error);
+      });
+  }
+  
+  function error() {
+    console.log("No se pudo obtener la ubicación.");
+  }
+  
+  window.addEventListener('load', getWeather);
+  
